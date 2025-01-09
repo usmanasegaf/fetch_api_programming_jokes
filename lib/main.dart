6dcb66610d1b7653b4388010eeb6124f5b3a0a7e
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2025-01-09 08:27:29 +0700

    first iteration

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/joke_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Programming Jokes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: JokeScreen(),
     );
   }
 }
